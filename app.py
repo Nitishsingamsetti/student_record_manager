b3d8749bf4bf76b36ec96f0b4eca69910705b816
nitishsingamsetti <nsingamsetti11@gmail.com>
2024-07-30 22:46:28 +0530

    htmlpage

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask,render_template,url_for,request
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['POST','GET'])
 def home():
-    
-    
-    
-    
-    
+    if request.method=='POST':
+        fname=request.form['fname']
+        lname=request.form['lname']
+        email=request.form['email']
+        passw=request.form['passw']
+        phno=request.form['phno']
     
     return render_template('index.html')
     
