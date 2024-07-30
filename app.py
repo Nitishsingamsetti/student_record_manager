ebb633c7d1c9eec4dee34cda03f9e4d3040ed945
nitishsingamsetti <nsingamsetti11@gmail.com>
2024-07-30 22:41:20 +0530

    htmlpage

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 from flask import Flask,render_template,url_for,request
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    
+    
+    
+    
+    
+    
+    return render_template('index.html')
+    
