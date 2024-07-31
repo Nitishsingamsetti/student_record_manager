f17f12060b5dd51044135b13354f9d106584c222
nitishsingamsetti <nsingamsetti11@gmail.com>
2024-07-31 12:32:31 +0530

    dbmsconnection

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 from flask import Flask,render_template,url_for,request
+import mysql.connector
 app=Flask(__name__)
+mydb=mysql.connector.connect(host='localhost',user='root',password='nitish',db='spm')
 
-@app.route('/',methods=['POST','GET'])
+@app.route('/')
 def home():
+    return render_template('index.html')
+
+@app.route('/sign',methods=['POST','GET'])
+def sign():
     if request.method=='POST':
+        print(request.form)
         fname=request.form['sfname']
         lname=request.form['slname']
         email=request.form['email']
         passw=request.form['passw']
         phno=request.form['phno']
+        print(fname,lname,email,passw,phno)
+        cursor=mydb.cursor(buffered=True)
+        cursor.execute('insert into stu_info(stu_fname,stu_lname,email,ph_no,password) values(%s,%s,%s,%s,%s)',[fname,lname,email,phno,passw])
+        mydb.commit()
+        cursor.close()
+        print('done')
+    return render_template('signup.html')
     
-    return render_template('index.html')
 
-app.run(debug=True)
+app.run(debug=True,use_reloader=True)
     
