de55bd6ebf75520a258adf078db39c18ab7a418e
nitishsingamsetti <nsingamsetti11@gmail.com>
2024-08-02 11:28:11 +0530

    verify

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask,render_template,url_for,request,redirect
 import mysql.connector
 from otp import genotp
 from cmail import sendmail
@@ -20,14 +20,20 @@ def sign():
         phno=request.form['phno']
         # print(fname,lname,email,passw,phno)
         otp=genotp()
-        subject="Welcome to sprm"
-        cursor=mydb.cursor(buffered=True)
-        cursor.execute('insert into stu_info(stu_fname,stu_lname,email,ph_no,password) values(%s,%s,%s,%s,%s)',[fname,lname,email,phno,passw])
-        mydb.commit()
-        cursor.close()
-        print('done')
+        subject="OTP verification for SPM application"
+        body=f'Registration otp for SPM application {otp}'
+        sendmail(to=email,subject=subject,body=body)
+        return redirect(url_for('verifyotp'))
+        # cursor=mydb.cursor(buffered=True)
+        # cursor.execute('insert into stu_info(stu_fname,stu_lname,email,ph_no,password) values(%s,%s,%s,%s,%s)',[fname,lname,email,phno,passw])
+        # mydb.commit()
+        # cursor.close()
+        # print('done')
         
     return render_template('signup.html')
+@app.route('/otp',methods=['GET','POST'])
+def verifyotp():
+    return render_template('verify.html')
     
 
 app.run(debug=True,use_reloader=True)
--- cmail.py
+++ cmail.py
@@ -2,7 +2,7 @@ import smtplib
 from smtplib import SMTP
 from email.message import EmailMessage
 def sendmail(to,body,subject):
-    server=smtplib.SMTP_SSL("smtp.gmail.com",587)
+    server=smtplib.SMTP_SSL("smtp.gmail.com",465)
     server.login("nsingamsetti11@gmail.com","qdiv ovua clvq kwfz")
     msg=EmailMessage()
     msg['FROM']="nsingamsetti11@gmail.com"
