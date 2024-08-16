39d77a6a5fb9d4160bd314b549f2acca032b4665
nitishsingamsetti <nsingamsetti11@gmail.com>
2024-08-16 12:36:23 +0530

    sqlnotes

---
+++ data.sql
@@ -0,0 +1,85 @@
+-- MySQL dump 10.13  Distrib 8.0.37, for Win64 (x86_64)
+--
+-- Host: localhost    Database: spm
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `notes`
+--
+
+DROP TABLE IF EXISTS `notes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notes` (
+  `notes_id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(20) NOT NULL,
+  `note_content` text,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `added_by` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`notes_id`),
+  UNIQUE KEY `title` (`title`),
+  KEY `added_by` (`added_by`),
+  CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`added_by`) REFERENCES `stu_info` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notes`
+--
+
+LOCK TABLES `notes` WRITE;
+/*!40000 ALTER TABLE `notes` DISABLE KEYS */;
+INSERT INTO `notes` VALUES (1,'champions legue','i am the goat of this league','2024-08-08 06:59:52','cr7@gmail.com'),(2,'laliga','24-25 champions\r\nvini ballondor winner\r\nBELLINGOAL\r\nrodrygo\r\nmbappe','2024-08-09 06:33:29','232niti@gmail.com');
+/*!40000 ALTER TABLE `notes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stu_info`
+--
+
+DROP TABLE IF EXISTS `stu_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stu_info` (
+  `stu_fname` varchar(50) NOT NULL,
+  `stu_lname` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `ph_no` bigint unsigned NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `password` varbinary(10) DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stu_info`
+--
+
+LOCK TABLES `stu_info` WRITE;
+/*!40000 ALTER TABLE `stu_info` DISABLE KEYS */;
+INSERT INTO `stu_info` VALUES ('nitish','singamsetti','232niti@gmail.com',12345678765,'2024-08-08 11:15:35',_binary '123456'),('ronaldo','cris','cr7@gmail.com',7070707,'2024-08-08 12:09:00',_binary '789456');
+/*!40000 ALTER TABLE `stu_info` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-08-16 12:33:25
